f2c29cfe2714b54c80b776dabe279fffe826f2d8
Mischa Tobias <mischa.tobias4b@gmail.com>
2024-12-06 13:13:28 -0600

    use public folder for images

--- src/ components/Carousel.jsx
+++ src/ components/Carousel.jsx
@@ -14,21 +14,21 @@ export const Carousel = () => {
     <div className="carousel-container">
       <Slider {...settings}>
         <div className="carousel-item">
-          <img src="/assets/gruporeuteam.jpg" alt="Image 1" />
+          <img src="./assets/gruporeuteam.jpg" alt="Image 1" />
           <div className="carousel-text">
             <h3>Item Title 1</h3>
             <p>Description of the item.</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src="/assets/photo2.jpg" alt="Image 2" />
+          <img src="./assets/photo2.jpg" alt="Image 2" />
           <div className="carousel-text">
             <h3>Item Title 2</h3>
             <p>Description of the item.</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src="/assets/photo3.jpg" alt="Image 2" />
+          <img src="./assets/photo3.jpg" alt="Image 2" />
           <div className="carousel-text">
             <h3>Item Title 3</h3>
             <p>Description of the item.</p>
